9f54b29720402f5549d794a36b8a851c2fa9c8ce
Ilma Aulia <87630364+ilmaaulia@users.noreply.github.com>
2025-02-13 10:36:32 +0700

    feat: navbar and hamburger components

---
+++ src/components/Hamburger/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from 'react-bootstrap'
+
+const Hamburger = ({ handleShow }) => {
+  return (
+    <Navbar expand="md" className="w-100 px-4 bg-primary text-white d-md-none">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleShow} />
+    </Navbar>
+  )
+}
+
+export default Hamburger
