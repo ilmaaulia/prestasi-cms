ff6953c6be0cc607a1689dd31a3671caa39026d4
Ilma Aulia <87630364+ilmaaulia@users.noreply.github.com>
2025-02-13 10:32:45 +0700

    feat: routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import './App.css'
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LoginPageAdmin from './pages/admin/login'
+import DashboardPageAdmin from './pages/admin/dashboard'
 
 function App() {
-
   return (
-    <></>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/admin/login" element={<LoginPageAdmin />} />
+        <Route path="/admin/dashboard" element={<DashboardPageAdmin />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
