c6083f690a3e76068fe6ad60ca2269f86092517b
Ilma Aulia <87630364+ilmaaulia@users.noreply.github.com>
2025-03-10 10:17:22 +0700

    style: adjust loading component margin

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -3,7 +3,7 @@ import { Container, Spinner } from 'react-bootstrap'
 
 const Loading = () => {
   return (
-    <Container className="text-center mt-5">
+    <Container className="text-center m-3">
       <div className="d-flex justify-content-center align-items-center gap-2">
         <Spinner animation="grow" variant="primary" size="sm" />
         <Spinner animation="grow" variant="primary" size="sm" />
