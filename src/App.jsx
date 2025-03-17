29b9181375ff50389bee35415a0741ba91e523f6
Ilma Aulia <87630364+ilmaaulia@users.noreply.github.com>
2025-03-17 11:52:21 +0700

    feat: routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPageAdmin from './pages/admin/login'
 import DashboardPageAdmin from './pages/admin/dashboard'
+import UsersPageAdmin from './pages/admin/users'
+import AchievementsPageAdmin from './pages/admin/achievements'
+import AchievementsCreatePage from './pages/admin/achievements/create'
+import NewsPageAdmin from './pages/admin/news'
 
 function App() {
   return (
@@ -9,6 +13,10 @@ function App() {
       <Routes>
         <Route path="/admin/login" element={<LoginPageAdmin />} />
         <Route path="/admin/dashboard" element={<DashboardPageAdmin />} />
+        <Route path="/admin/users" element={<UsersPageAdmin />} />
+        <Route path="/admin/achievements" element={<AchievementsPageAdmin />} />
+        <Route path="/admin/achievements/create" element={<AchievementsCreatePage />} />
+        <Route path="/admin/news" element={<NewsPageAdmin />} />
       </Routes>
     </BrowserRouter>
   )
