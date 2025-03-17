4f811ad8c72e186fca320be712bbd45fda6053fe
Ilma Aulia <87630364+ilmaaulia@users.noreply.github.com>
2025-03-17 11:39:02 +0700

    feat: achievement page

---
+++ src/pages/admin/achievements/index.jsx
@@ -0,0 +1,116 @@
+import React, { useEffect } from 'react'
+import { Container, Table } from 'react-bootstrap'
+import Breadcrumbs from '../../../components/Breadcrumb'
+import AppButton from '../../../components/Button'
+import SidebarAdmin from '../../../components/SidebarAdmin'
+import Loading from '../../../components/Loading'
+import Hamburger from '../../../components/Hamburger'
+import { config } from '../../../config'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
+const AchievementsPageAdmin = () => {
+  const token = localStorage.getItem('token')
+  const [show, setShow] = React.useState(false)
+  const [data, setData] = React.useState([])
+  const [loading, setLoading] = React.useState(true)
+
+  const navigate = useNavigate()
+  
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+  const handleLogout = () => {
+    localStorage.removeItem('token')
+    window.location.href = '/admin/login'
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`${config.api_host_dev}/achievements`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+
+        setData(res.data.data)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  return (
+    <div className="d-flex vh-100">
+      <SidebarAdmin show={show} handleClose={handleClose} handleLogout={handleLogout} />
+
+      <Container fluid className="d-flex flex-column p-0 vh-100 overflow-hidden">
+        <Hamburger handleShow={handleShow} />
+        <main className="p-5 vh-100 overflow-auto">
+          <h1 className="fs-3">Prestasi</h1>
+          <Breadcrumbs 
+            textSecond='Prestasi'
+          />
+          <AppButton className="mb-2" action={() => navigate('/admin/achievements/create')}>Tambah</AppButton>
+          <Table responsive striped bordered hover className="w-100">
+            <thead className="text-center">
+              <tr>
+                <th>No</th>
+                <th>Nama Prestasi</th>
+                <th>Tanggal</th>
+                <th>Kelompok Kegiatan</th>
+                <th>Jenis Kegiatan</th>
+                <th>Jenis Prestasi</th>
+                <th>Tingkat Kompetisi</th>
+                <th>Bukti Prestasi</th>
+                <th>Status</th>
+                <th>Nama Mahasiswa</th>
+                <th>Aksi</th>
+              </tr>
+            </thead>
+            <tbody>
+              {loading ? (
+                <tr>
+                  <td colSpan="10" className="text-center">
+                    <Loading />
+                  </td>
+                </tr>
+              ) : data.length > 0 ? (
+                data.map((data, index) => (
+                  <tr key={data._id}>
+                    <td className="text-center">{index + 1}</td>
+                    <td>{data.name}</td>
+                    <td>{new Date(data.date).toLocaleDateString()}</td>
+                    <td>{data.activity_group}</td>
+                    <td>{data.activity_type}</td>
+                    <td>{data.achievement_type}</td>
+                    <td>{data.competition_level}</td>
+                    <td>
+                      <img src={`${config.image_base_url}/${data.image?.name}`} alt="Bukti Prestasi" width={150}/>
+                    </td>
+                    <td className="text-center">{data.status}</td>
+                    <td>{`${data.student.firstName} ${data.student.lastName}`}</td>
+                    <td className="d-flex gap-2 flex-wrap">
+                      <button className="btn btn-primary">Edit</button>
+                      <button className="btn btn-danger">Hapus</button>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="10" className="text-center">Tidak ada data</td>
+                </tr>
+              )}
+            </tbody>
+          </Table>
+        </main>
+      </Container>
+    </div>
+  )
+} 
+
+export default AchievementsPageAdmin
