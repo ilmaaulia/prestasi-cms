2fe1891a6acbb02be6d9ee82f6e61ee20148b08c
Ilma Aulia <87630364+ilmaaulia@users.noreply.github.com>
2025-03-17 11:17:56 +0700

    feat: image base url

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
 const config = {
   api_host_dev : import.meta.env.VITE_API_HOST_DEV,
   api_host_prod : import.meta.env.VITE_API_HOST_PROD,
+  image_base_url: import.meta.env.VITE_IMAGE_BASE_URL,
 }
 
 export { config }
