9436113ac38d582d95b7fc7b51e06078a06cca32
Ilma Aulia <87630364+ilmaaulia@users.noreply.github.com>
2025-04-11 12:44:45 +0700

    feat: integrate redux provider to manage application state

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import store from './redux/store.js'
 
 import './index.css'
 import App from './App.jsx'
@@ -7,6 +9,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
