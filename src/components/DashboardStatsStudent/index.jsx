9d07159288aff1edd3b0aa0a0af5bf40f3345724
Ilma Aulia <87630364+ilmaaulia@users.noreply.github.com>
2025-04-22 05:41:51 +0700

    feat: dashboard stats component for dashboard student

---
+++ src/components/DashboardStatsStudent/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Card, Badge, Col, Row } from 'react-bootstrap'
+
+const DashboardStatsStudent = ({ achievements, getRelevantTags, tagColors }) => {
+  return (
+    <>
+      <Row className="mb-4">
+        <Col xs={12} md={4} className="mb-3">
+          <Card className="h-100 d-flex flex-column shadow-sm bg-pastel-blue border-0">
+            <Card.Body className="d-flex flex-column justify-content-between">
+              <Card.Title>Total Prestasi</Card.Title>
+              <Card.Text className="display-4">{achievements.length}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col xs={12} md={4} className="mb-3">
+          <Card className="h-100 d-flex flex-col</Col>umn shadow-sm bg-pastel-green border-0">
+            <Card.Body className="d-flex flex-column justify-content-between">
+              <Card.Title>Ragam Prestasi Diraih</Card.Title>
+              <div className="d-flex flex-wrap gap-2 mt-2">
+                {getRelevantTags().length > 0 ? (
+                  getRelevantTags().map((tag, i) => (
+                    <Badge key={i} bg={tagColors[i % tagColors.length]}>
+                      {tag}
+                    </Badge>
+                  ))
+                ) : (
+                  <p className="text-muted">Belum ada prestasi</p>
+                )}
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  )
+}
+
+export default DashboardStatsStudent
