95c614152ed3b030ffabe7bf3d662f38afa1c1c3
Ilma Aulia <87630364+ilmaaulia@users.noreply.github.com>
2025-04-24 10:21:51 +0700

    feat: student profile page

--- src/routes/ProfileStudentRoute.jsx
+++ src/routes/ProfileStudentRoute.jsx
@@ -1,8 +1,9 @@
 import { Route, Routes } from 'react-router-dom'
+import ProfilePage from '../pages/students/profile'
 
 const ProfileStudentRoute = () => (
   <Routes>
-    <Route index element={<div>Halaman profil mahasiswa belum tersedia</div>} />
+    <Route index element={<ProfilePage />} />
   </Routes>
 )
 
