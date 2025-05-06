da10c476c6405de5baab39a0ef55e691f70f86df
Ilma Aulia <87630364+ilmaaulia@users.noreply.github.com>
2025-05-06 10:23:45 +0700

    fix: alert message props

--- src/components/AlertMessage/index.jsx
+++ src/components/AlertMessage/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Alert } from 'react-bootstrap'
 
-const AlertMessage = ({ variant, message }) => {
-  return <Alert variant={variant}>{message}</Alert>
+const AlertMessage = ({ type, message }) => {
+  return <Alert variant={type}>{message}</Alert>
 }
 
 export default AlertMessage
