77ba17e1f497f13d0117ee9c20b2134687a56211
Ilma Aulia <87630364+ilmaaulia@users.noreply.github.com>
2025-05-06 10:23:08 +0700

    feat: update redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,6 +8,8 @@ import {
 import { thunk } from 'redux-thunk'
 import authReducer from './auth/reducer'
 import achievementsReducer from './achievements/reducer'
+import newsReducer from './news/reducer'
+import usersReducer from './users/reducer'
 import notifReducer from './notif/reducer'
 
 const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
@@ -15,6 +17,8 @@ const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const rootReducers = combineReducers({
   auth: authReducer,
   achievements: achievementsReducer,
+  news: newsReducer,
+  students: usersReducer,
   notif: notifReducer,
 })
 
