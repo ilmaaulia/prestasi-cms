45366b5dd55c98a44490a48045581a24222920bc
Ilma Aulia <87630364+ilmaaulia@users.noreply.github.com>
2025-05-21 15:38:39 +0700

    refactor: simplify news fetching

--- src/components/LatestNews/index.jsx
+++ src/components/LatestNews/index.jsx
@@ -5,17 +5,13 @@ import Cards from '../Cards'
 const LatestNews = () => {
   const [news, setNews] = useState([])
 
-  useEffect(() => {
-    const fetchnews = async () => {
-      try {
-        const response = await getData('/newses?sort=date:desc&limit=3')
-        setNews(response.data.data)
-      } catch (error) {
-        console.error('Error fetching news:', error)
-      }
-    }
+  const fetchLatestNews = async () => {
+    const response = await getData('/newses?sort=date:desc&limit=3')
+    setNews(response.data.data)
+  }
 
-    fetchnews()
+  useEffect(() => {
+    fetchLatestNews()
   }, [])
 
   return <Cards data={news} title="Berita Terbaru" />
